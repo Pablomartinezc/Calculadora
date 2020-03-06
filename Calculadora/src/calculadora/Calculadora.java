356826f6cad52c2199ec4849df43ffe2ebb8b42e
dam <dam@pc.educarm.net>
2020-03-06 15:35:13 +0100

    Primeros archivosCAptura C

---
+++ Calculadora/src/calculadora/Calculadora.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author redes
+ * @version version 1.0
+ * @since 28/02/2020
+ * 
+ */
+public class Calculadora {
+    private int num1;
+    private int num2;
+    /**
+     * 
+     * @param a
+     * @param b 
+     */
+    public Calculadora(int a, int b){
+        num1=a;
+        num2=b;
+    }
+    /**
+     * 
+     * @return resultado de la suma
+     */
+    
+    public int suma(){
+        int result=num1+num2;
+        return result;
+    }
+    /**
+     * 
+     * @return resultado de la resta
+     */
+    public int resta(){
+        int result;
+        if (resta2())   result = num1-num2;
+        else            result = num2-num1;
+        return result;
+    }
+    /**
+     * 
+     * @return 
+     */    
+    public boolean resta2(){
+        return num1 > num2;
+    }
+    /**
+     * 
+     * @return resultado de la multiplicacion
+     */
+    public int multiplica(){
+        int result=num1*num2;
+        return result;
+    }
+    /**
+     * 
+     * @return resultado de la division
+     * @deprecated por divide2
+     */
+    public int divide(){
+        int result=num1/num2;
+        return result;
+    }
+    /**
+     * 
+     * @return resultado de la division
+     */
+    public Integer divide2(){
+        if(num2==0) return null;
+        int result=num1/num2;
+        return result;
+    }
+    /**
+     * 
+     * @return result
+     * @exception java.lang.ArithmeticException si divide por 0
+     */
+     public int divide0(){
+        if(num2==0) 
+            throw new java.lang.ArithmeticException("División por 0");
+        else{
+            int result=num1/num2;
+            return result;
+        }
+    }
+     
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Calculadora calcu = new Calculadora(20, 10);
+        System.out.println(calcu.suma());
+    }
+}
